117fea4ac945ac9f0355081a06e3b4ab2fb1731c
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-19 10:06:09 +0530

    add method for add items to the list

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ from flask import Flask, session, render_template, request, g
 app = Flask(__name__)
 
 app.secret_key = "DOG-4567456988-VS-CAT"
+app.config["SESSION_COOKIE_NAME"] = "MY-COOKIE-666"
 
 
-@app.route('/')
+@app.route("/", methods=["POST", "GET"])
 def index():
-    data = get_database()
-    return render_template('index.html', execute_data=data)
-    #return data[0]
+    session["data"], session["random_items"] = get_database()
+    return render_template('index.html', execute_data=session["data"], shopping_list=session["random_items"])
+    # return data[0]
 
 
 @app.route("/add_items", methods=["post"])
 def add_items():
-    return request.form["select_items"]
-
-
+    session["random_items"].append(str(request.form["select_items"]))
+    return render_template('index.html', execute_data=session["data"], shopping_list=session["random_items"])
 
 
 def get_database():
@@ -31,7 +31,11 @@ def get_database():
         cursor.execute('SELECT name FROM groceries')
         execute_data = cursor.fetchall()
         execute_data = [str(val[0]) for val in execute_data]
-    return execute_data
+
+        shopping_list = execute_data.copy()
+        random.shuffle(shopping_list)
+        shopping_list = shopping_list[:5]
+    return execute_data, shopping_list
 
 
 @app.teardown_appcontext
@@ -39,16 +43,6 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
