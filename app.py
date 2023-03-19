9a2f0679eba0b449309eb68eba5b1e455a914cc5
D.J Amarasinghe <74134064+Deshan555@users.noreply.github.com>
2023-03-19 08:51:16 +0530

    create new route name add_items

--- app.py
+++ app.py
@@ -12,8 +12,15 @@ app.secret_key = "DOG-4567456988-VS-CAT"
 @app.route('/')
 def index():
     data = get_database()
-    #return render_template('index.html')
-    return str(data)
+    return render_template('index.html', execute_data=data)
+    #return data[0]
+
+
+@app.route("/add_items", methods=["post"])
+def add_items():
+    return request.form["select_items"]
+
+
 
 
 def get_database():
@@ -21,8 +28,10 @@ def get_database():
     if db is None:
         db = g._database = sqlite3.connect('grocery_list.db')
         cursor = db.cursor()
-        cursor.execute('SELECT * FROM groceries')
-    return cursor.fetchall()
+        cursor.execute('SELECT name FROM groceries')
+        execute_data = cursor.fetchall()
+        execute_data = [str(val[0]) for val in execute_data]
+    return execute_data
 
 
 @app.teardown_appcontext
